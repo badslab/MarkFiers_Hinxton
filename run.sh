a6273342c74898e0c123ec3b5d957ec7043fd28f
Mark Fiers <mark.fiers.42@gmail.com>
2026-03-23 14:50:50 +0100

    Create run.sh for updating and restarting process
    Add a script to pull updates and restart if necessary

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BEFORE=$(git rev-parse @)
+git pull --ff-only origin
+AFTER=$(git rev-parse @)
+
+if [ "$BEFORE" != "$AFTER" ]; then
+    echo "⚠️  Updated from remote — restarting..." >&2
+    exec "/home/training/course_dir/data_dir/MarkFiers/run.sh" "$@"
+fi
+
+# Safe to continue — already was up to date
+
+echo "Start"
